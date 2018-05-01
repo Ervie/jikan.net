2440c8c718c001477fcec6b4127fe35339297cd3
Ervin <bbuchala93@gmail.com>
2018-05-01 12:13:47 +0200

    Basic project structure.

---
+++ JikanDotNet/JikanDotNet/Interfaces/IJikan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JikanDotNet
+{
+    interface IJikan
+    {
+		void GetPerson(long id);
+
+		void GetAnime(long id);
+
+		void GetManga(long id);
+
+		void GetCharacter(long id);
+    }
+}
---
+++ JikanDotNet/JikanDotNet/Jikan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace JikanDotNet
+{
+	public class Jikan : IJikan
+	{
+		public void GetAnime(long id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void GetCharacter(long id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void GetManga(long id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void GetPerson(long id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
