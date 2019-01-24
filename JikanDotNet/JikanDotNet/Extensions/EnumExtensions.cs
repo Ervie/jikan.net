2b32ddb81387338e908e95fb1a604432c09b3568
Rodolfo Robles <rjroblesq@gmail.com>
2019-01-23 19:51:58 -0600

    More elegant code

--- JikanDotNet/JikanDotNet/Extensions/EnumExtensions.cs
+++ JikanDotNet/JikanDotNet/Extensions/EnumExtensions.cs
@@ -1,4 +1,4 @@
-﻿using JikanDotNet.Resources;
+using JikanDotNet.Resources;
 using System;
 using System.ComponentModel;
 using System.Reflection;
@@ -7,26 +7,15 @@ namespace JikanDotNet.Extensions
 {
 	internal static class EnumExtensions
 	{
-		public static string GetDescription<T>(this T enumerationValue)
-	where T : struct
+		public static string GetDescription<T>(this Enum source)
 		{
-			Type type = enumerationValue.GetType();
-			if (!type.IsEnum)
-			{
-				throw new ArgumentException(Errors.NotEnum, "enumerationValue");
-			}
-			
-			MemberInfo[] memberInfo = type.GetMember(enumerationValue.ToString());
-			if (memberInfo != null && memberInfo.Length > 0)
-			{
-				object[] attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
-
-				if (attrs != null && attrs.Length > 0)
-				{
-					return ((DescriptionAttribute)attrs[0]).Description;
-				}
-			}
-			return enumerationValue.ToString();
+			return
+				source
+					.GetType()
+					.GetMember(source.ToString())
+					.FirstOrDefault()
+					?.GetCustomAttribute<DescriptionAttribute>()
+					?.Description;
 		}
 	}
 }
