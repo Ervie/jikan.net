6b3571ae73141bbca9a214a628220e2c923a14a3
Ervie <bbuchala93@gmail.com>
2019-07-28 18:26:56 +0200

    Created classes/enums for advanced user manga list request.

---
+++ JikanDotNet/JikanDotNet/Enumerations/UserListMangaPublishingStatus.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace JikanDotNet
+{
+	/// <summary>
+	/// Status value for user list manga filtering.
+	/// </summary>
+	public enum UserListMangaPublishingStatus
+	{
+		/// <summary>
+		/// Allow all statuses to be displayed in results.
+		/// </summary>
+		[Description("")]
+		NoFilter,
+
+		/// <summary>
+		/// Publishing status.
+		/// </summary>
+		[Description("publishing")]
+		Publishing,
+
+		/// <summary>
+		/// Finished status.
+		/// </summary>
+		[Description("finished")]
+		Finished,
+
+		/// <summary>
+		/// To be published status.
+		/// </summary>
+		[Description("to_be_published")]
+		ToBePublished
+	}
+}
---
+++ JikanDotNet/JikanDotNet/Model/Search/UserListMangaSearchConfig.cs
@@ -0,0 +1,69 @@
+﻿using JikanDotNet.Extensions;
+using JikanDotNet.Interfaces;
+using System.Text;
+
+namespace JikanDotNet
+{
+	/// <summary>
+	/// Model class of search configuration for advanced manga user list search.
+	/// </summary>
+	public class UserListMangaSearchConfig : ISearchConfig
+	{
+		/// <summary>
+		/// Order items with respect to a property
+		/// </summary>
+		public UserListMangaSearchSortable OrderBy { get; set; }
+
+		/// <summary>
+		/// Order items with respect to a second property
+		/// </summary>
+		public UserListMangaSearchSortable OrderBy2 { get; set; }
+
+		/// <summary>
+		/// Define sort direction for <see cref="OrderBy">OrderBy</see> property.
+		/// </summary>
+		public SortDirection SortDirection { get; set; }
+
+		/// <summary>
+		/// Filter manga by this Magazine ID.
+		/// </summary>
+		public long MagazineId { get; set; }
+
+		/// <summary>
+		/// Filter manga with a status.
+		/// </summary>
+		public UserListMangaPublishingStatus PublishingStatus { get; set; }
+
+		/// <summary>
+		/// Create query from current parameters for search request.
+		/// </summary>
+		/// <returns>Query from current parameters for search request</returns>
+		public string ConfigToString()
+		{
+			StringBuilder builder = new StringBuilder();
+
+			if (OrderBy != UserListMangaSearchSortable.NoSorting)
+			{
+				builder.Append($"&order_by={OrderBy.GetDescription()}");
+				builder.Append($"&sort={SortDirection.GetDescription()}");
+
+				if (OrderBy2 != UserListMangaSearchSortable.NoSorting)
+				{
+					builder.Append($"&order_by2={OrderBy2.GetDescription()}");
+				}
+			}
+
+			if (MagazineId > 0)
+			{
+				builder.Append($"&magazine={MagazineId}");
+			}
+
+			if (PublishingStatus != UserListMangaPublishingStatus.NoFilter)
+			{
+				builder.Append($"&publishing_status={PublishingStatus.GetDescription()}");
+			}
+
+			return builder.ToString().TrimEnd('&');
+		}
+	}
+}
