f438ff76ca2b95e14d1881dd11adf17a42ef459a
Ervie <bartlomiejbuchala@Mac-mini-Bartomiej.local>
2022-02-02 21:25:46 +0100

    Update package version to 2.0, extend targetted frameworks

--- JikanDotNet/Consts/ParameterConsts.cs
+++ JikanDotNet/Consts/ParameterConsts.cs
@@ -1,7 +1,13 @@
 namespace JikanDotNet.Consts
 {
+    /// <summary>
+    /// Constants values related to limit of api.
+    /// </summary>
     public static class ParameterConsts
     {
+        /// <summary>
+        /// Maximum page size for general requests
+        /// </summary>
         public const int MaximumPageSize = 25;
     }
 }
