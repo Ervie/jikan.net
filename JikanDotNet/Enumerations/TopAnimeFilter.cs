5f41fe21f0327c15f19ea1801e87d233497d80a2
vynxcc <svynxc@gmail.com>
2023-01-06 13:55:28 -0500

    Added Top Anime Filter

---
+++ JikanDotNet/Enumerations/TopAnimeFilter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace JikanDotNet.Enumerations
+{
+    /// <summary>
+	/// Defines filter used in top anime request
+	/// </summary>
+    public enum TopAnimeFilter
+    {
+        /// <summary>
+		/// Filter by airing
+		/// </summary>
+		[Description("airing")]
+        Airing,
+
+        /// <summary>
+        /// Filter by upcoming
+        /// </summary>
+        [Description("upcoming")]
+        Upcoming,
+
+        /// <summary>
+        /// Filter by popularity
+        /// </summary>
+        [Description("popularity")]
+        ByPopularity,
+
+        /// <summary>
+        /// Filter by favorites
+        /// </summary>
+        [Description("favorites")]
+        Favorite,
+        /// <summary>
+        /// Use no filter
+        /// </summary>
+        [Description("none")]
+        None
+    }
+}
